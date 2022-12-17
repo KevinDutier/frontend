52a9b3a788aafc32316284924080cc91e24e17f9
KevinDutier desktop <kevin.dutier@gmail.com>
2022-12-17 18:06:03 +0100

    restored carousel

--- components/Article.js
+++ components/Article.js
@@ -47,43 +47,18 @@ export default function Article() {
     <div className={styles.main}>
       {/* LEFT SECTION (contains carousel) */}
       <div className={styles.left}>
-        {/* <Carousel
+        <Carousel
           className={styles.carousel}
           autoPlay={false} // disables autoplay
           transitionTime="700" // how long is the animation
           infiniteLoop={true} // loops back to first image after reaching the last one
           showStatus={false} // hides "1 of 3" in the corner
           showThumbs={false} // hides thumbnails
-          // thumbWidth={64}
           swipeable={true} // allows user to drag images with the mouse (default: true)
           emulateTouch={true} // enables swipe on non-touch screens
           useKeyboardArrows={true} // enables user to use keyboard arrows
         >
           {images}
-        </Carousel> */}
-        <Carousel
-          className={styles.carousel}
-          autoPlay={true}  // enables autoplay
-          interval="4000"  // how long before showing next slide
-          transitionTime="700"  // how long is the animation
-          infiniteLoop={true}  // loops back to first image after reaching the last one
-          showStatus={false}  // hides "1 of 3" in the corner
-          showThumbs={false}  // hides thumbnails
-          swipeable={true}  // allows user to drag images with the mouse (default: true)
-          emulateTouch={true}  // enables swipe on non-touch screens
-          stopOnHover={false}  // stops slideshow on hover
-          useKeyboardArrows={true}  // enables user to use keyboard arrows
-          onClickItem={(index) => clickBanner(index)}
-        >
-          <div>
-            <img src="/banners/banner0.jpg" />
-          </div>
-          <div>
-            <img src="/banners/banner1.jpg" />
-          </div>
-          <div>
-            <img src="/banners/banner2.jpg" />
-          </div>
         </Carousel>
       </div>
 
